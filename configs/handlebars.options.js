6a67e2c86aa5cb759dd879581125e148ea8dadd6
Andrew McPherson <andrewmcp333@gmail.com>
2014-07-31 18:30:39 -0400

    Restyled the layout for pages other than home.

--- configs/handlebars.options.js
+++ configs/handlebars.options.js
@@ -1,4 +1,3 @@
 module.exports.layoutsDir = "./layouts";
 module.exports.defaultLayout = "default.layout.handlebars";
-module.exports.defaultLayout = "development.layout.handlebars";
 module.exports.partialsDir = "./partials";
